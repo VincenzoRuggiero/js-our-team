6224e7ee277b9de4bfbebd5b2a07ff3455350a8f
VincenzoRuggiero <vincenzoru@live.it>
2022-12-05 14:13:05 +0100

    Create Team's array objects

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder &amp: CEO",
+    profilePic: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    profilePic: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    profilePic: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    profilePic: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    profilePic: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    profilePic: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+for (let i = 0; i < team.length; i++) {
+  console.log(team[i]);
+}
